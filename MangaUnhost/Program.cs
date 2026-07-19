02095737dd8be609f7e8d8c3cec6f37be8a6d98d
Marcus André <ddtank.marcus@gmail.com>
2026-07-19 03:39:30 -0300

    Fix missing System.Threading using directive

--- MangaUnhost/Program.cs
+++ MangaUnhost/Program.cs
@@ -13,6 +13,7 @@ using System.Net;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace MangaUnhost
